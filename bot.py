100161e3ddc4da3b5791b8662258a100fda410fc
cwri463 <125087776+cwri463@users.noreply.github.com>
2025-07-09 22:18:40 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,25 @@
-placeholder
+import os
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.guilds = True
+intents.members = True
+
+bot = commands.Bot(command_prefix="/", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user.name}")
+
+# Load command modules from ./cogs
+@bot.event
+async def setup_hook():
+    await bot.load_extension("cogs.tasks")  # Load tasks feature
+
+bot.run(TOKEN)
